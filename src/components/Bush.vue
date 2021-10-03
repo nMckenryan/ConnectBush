dc804f5d1240745cdb644a00d87271340a9e50ef
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-10-03 18:30:47 +1100

    Changed the Social Media Icons via Tailwind Toolbox  and implemented some font changes to improve responsiveness

--- src/components/Bush.vue
+++ src/components/Bush.vue
@@ -12,32 +12,23 @@
       <div :class="this.textcolor" style="font-family: 'Merriweather', serif">
         <!-- TODO: Port logo to SVG for color changability  -->
         <img
-          class="
-            mx-auto
-            h-32
-            transform
-            scale-75
-            md:scale-100
-            rounded-3xl
-            shadow-lg
-            mt-2
-          "
+          class="mx-auto h-32 transform scale-75 md:scale-100 rounded-3xl mt-2"
           src="../assets/logo.png"
           alt="logo"
         />
         <!-- TITLE -->
-        <h1 class="text-2xl md:text-4xl font-extrabold md:mt-2 -m-5 md:m-3">
+        <h1 class="text-xl md:text-4xl font-extrabold md:mt-2 -m-5 md:m-3">
           {{ title }}
         </h1>
         <!-- SUBTITLE -->
-        <h4 class="text-lg md:text-xl mt-2 p-3 md:-mt-4">
+        <h4 class="text-sm md:text-lg mt-2 p-3 md:-mt-4">
           {{ msg }}
         </h4>
       </div>
 
       <!-- Link Columns -->
       <div
-        class="flex-col mx-auto w-2/4 flex-shrink m-1 p-1 w-240"
+        class="flex-col mx-auto w-full md:w-2/4 flex-shrink m-1 p-1 w-240"
         id="linkCol"
         v-for="details in links"
         v-bind:key="details.message"
@@ -52,11 +43,11 @@
           >
             <!-- Icon  & Message-->
             <img
-              class="h-16 w-16 m-2 p-3 rounded-xl bg-white"
+              class="h-10 w-10 md:h-16 md:w-16 m-2 p-2 rounded-xl bg-white"
               v-bind:src="images.url"
               v-bind:alt="logo"
             />
-            <p class="flex my-auto text-baseline md:text-lg text-left ml-2">
+            <p class="flex my-auto text-sm md:text-lg text-left ml-2">
               {{ details.CTA }}
             </p>
           </a>
@@ -64,21 +55,90 @@
       </div>
 
       <!-- Social Media Favicons -->
-      <!-- TODO: change icon background on for better contrast -->
+
       <div :class="iconcolor">
-        <a href="https://www.facebook.com/"
-          ><i class="fab fa-facebook-square fa-3x"></i
-        ></a>
-        <a href=""><i class="fab fa-snapchat-square fa-3x"></i></a>
-        <a href="https://twitter.com/"
-          ><i class="fab fa-twitter-square fa-3x"></i
+        <!-- FACEBOOK -->
+        <a href="https://www.facebook.com"
+          ><svg
+            class="h-8 w-8"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+            />
+          </svg>
+        </a>
+
+        <!-- GITHUB -->
+        <a href="https://www.github.com/"
+          ><svg
+            class="h-8 w-8"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+            /></svg
         ></a>
 
-        <a href="https://www.youtube.com/channel/"
-          ><i class="fab fa-youtube-square fa-3x"></i
+        <!-- INSTAGRAM -->
+
+        <a href="https://www.instagram.com"
+          ><svg
+            class="h-8 w-8"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg
         ></a>
-        <a href="https://www.google.com"
-          ><i class="fas fa-envelope-square fa-3x"></i
+
+        <!-- TWITTER -->
+        <a href="https://twitter.com/"
+          ><svg
+            class="h-8 w-8"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+            /></svg
+        ></a>
+
+        <!-- LINKEDIN  -->
+        <a href="https://www.linkedin.com/"
+          ><svg
+            class="h-8 w-8"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+            />
+            <rect x="2" y="9" width="4" height="12" />
+            <circle cx="4" cy="4" r="2" /></svg
         ></a>
       </div>
     </div>
@@ -121,14 +181,18 @@ export default {
         //10 colors
         // 0 = main color, 1 = gradient end, 2 = contrasting window color. 3 = text
         ["trollOrange", "trollOrange", "sonsBlue", "white"],
-        ["adMech", "white", "screamGrey", "black"],
+        ["adMech", "white", "screamGrey", "white"],
         ["screamGrey", "adMech", "nightLord", "white"],
         ["nightLord", "sonsBlue", "black", "white"],
       ];
 
       // Selects a Color Scheme at random
       var scheme = colorArr[_.random(0, 3)];
-      this.iconcolor = "flex mx-auto items-center border text-" + scheme[2];
+      this.iconcolor =
+        "flex mx-auto items-center rounded-lg p-1 bg-" +
+        scheme[2] +
+        " text-" +
+        scheme[3];
       this.bgcolor =
         "bg-gradient-to-t from-" +
         scheme[0] +
