591da75b0a657a88bc775ecfbce156015ad32929
nmckenryan <n.mckenryan@gmail.com>
2024-10-08 02:21:02 +1100

    trying to connect to airtable again

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,20 @@
 <template>
       <!--  HEADER IMAGE-->
-      <div class="flex flex-col flex-shrink mx-auto bg-white w-1/4 rounded-xl my-5 shadow-md" style="font-family: 'Merriweather', serif">
+      <div class="flex flex-col flex-shrink mx-auto  w-1/4 rounded-xl my-5 shadow-md" style="font-family: 'Merriweather', serif">
         <img
           class="mx-auto h-32 transform scale-75 -mb-5"
-          src="../assets/logoBlack.png"
+          src="../assets/logoWhite.png"
           alt="logo"
         />
         <!-- TITLE -->
         <h1
-          class="text-lg md:text-3xl font-extrabold text-shadow text-black"
+          class="text-lg md:text-3xl font-extrabold text-shadow text-white"
           data-test="title"
         >
           {{ title }}
         </h1>
         <!-- SUBTITLE -->
-        <h4 class="text-md md:text-lg text-shadow text-black" data-test="subtitle">
+        <h4 class="text-md md:text-lg text-shadow text-white" data-test="subtitle">
           {{ msg }}
         </h4>
       </div>
@@ -25,16 +25,6 @@
     export default {
     name: "Header",
     props: ["msg", "title"],
-    data() {
-        return {
-        links: [],
-        credentials: process.env.VUE_APP_AT_LINK,
-        bgcolor: "",
-        subcolor: "",
-        iconcolor: "",
-        textcolor: "",
-        };
-    },
 
     };
 </script>
