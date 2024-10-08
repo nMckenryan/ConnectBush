db6340a076a1b883f089015225546c392b03eacd
nMckenryan <n.mckenryan@gmail.com>
2024-10-08 19:15:28 +1100

    MAJOR UPDATE: Changed build handler to use Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 // tailwind.config.js
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
