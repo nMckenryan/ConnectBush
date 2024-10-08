f512c575d05042259e892ee14036f057876938a7
nMckenryan <n.mckenryan@gmail.com>
2024-10-08 19:37:50 +1100

    Implemented simple spinner  Dialogue for column

--- src/components/Columns.vue
+++ src/components/Columns.vue
@@ -1,51 +1,48 @@
 <template>
-  <div class="columnsView">
-    <!-- Link Columns -->
-    <div v-if="links?.length > 0">
-      <div
+  <div v-if="!loading" class="columnsView">
 
+    <div v-if="links.length > 0">
+      <div
         id="linkCol"
         v-for="details in links"
-        v-bind:key="details.message"
+        v-bind:key="details.CTA"
+        class="flex-shrink mx-auto w-full md:w-3/4 m-3 p-1 bg-white rounded-lg shadow-md"
       >
-        <div class="flex-shrink mx-auto w-full md:w-3/4 m-3 p-1  bg-white rounded-lg shadow-md">
-          <a
-            class="flex"
-            v-bind:href="details.Link"
-            v-for="images in details.Thumbnails"
-            v-bind:key="images.url"
-            style="fill: green !important"
+        <a
+          class="flex"
+          v-bind:href="details.Link"
+          v-for="images in details.Thumbnails"
+          v-bind:key="images.url"
+          style="fill: green !important"
+        >
+          <img
+            class="h-10 w-10 md:h-10 md:w-10 m-2 p-2 rounded-xl bg-white"
+            v-lazy="images.url"
+            alt="logo"
+          />
+          <p
+            class="flex my-auto text-sm md:text-lg text-left ml-2 text-black"
+            data-test="cta"
+            id="linkText"
           >
-            <!-- Icon  & Message-->
-            <img
-              class="h-10 w-10 md:h-10 md:w-10 m-2 p-2 rounded-xl bg-white"
-              v-bind:src="images.url"
-              alt="logo"
-            />
-            <p
-              class="flex my-auto text-sm md:text-lg text-left ml-2 text-black"
-              data-test="cta"
-              id="linkText"
-            >
-              {{ details.CTA }}
-            </p>
-          </a>
-        </div>
+            {{ details.CTA }}
+          </p>
+        </a>
       </div>
     </div>
 
-    <!-- Fallback if no links available -->
+
     <div v-else>
       <div
         class="mx-auto w-full md:w-2/4 flex-shrink m-1 p-1 bg-white rounded-lg shadow-md"
         id="linkCol"
       >
         <div class="flex flex-row">
-            <img
-              class="h-10 w-10 md:h-10 md:w-10 m-2 p-2 rounded-xl bg-transparent"
-              src="../assets/logoBlack.png"
-              alt="logo"
-            />
+          <img
+            class="h-10 w-10 md:h-10 md:w-10 m-2 p-2 rounded-xl bg-transparent"
+            v-lazy="'../assets/logoBlack.png'"
+            alt="logo"
+          />
           <p
             class="flex my-auto text-sm md:text-baseline text-left ml-2 text-black"
             data-test="fallbackCta"
@@ -57,14 +54,23 @@
       </div>
     </div>
   </div>
+  <div class="flex justify-center" v-else>
+
+    <intersecting-circles-spinner
+      :size="100"
+      color="#ffffff"
+    />
+
+  </div>
 </template>
 
 <script>
-
+import { IntersectingCirclesSpinner } from 'epic-spinners'
 
 
 export default {
   name: "ColumnView",
+  components: { IntersectingCirclesSpinner },
   async mounted() {
     const url = import.meta.env.VITE_URL;
     const token = import.meta.env.VITE_PA_TOKEN;
@@ -79,15 +85,18 @@ export default {
       this.links = data.records.map(record => record.fields);
     } catch (error) {
       console.error(error);
+    } finally {
+      this.loading = false;
     }
   },
-
   data() {
     return {
+      loading: true,
       links: [],
     };
   },
 };
 </script>
+
 
 
